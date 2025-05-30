ed664f4c8eb4b95f0795d1d7a5d667d79cdb20c0
elzuzu <45237180+elzuzu@users.noreply.github.com>
2025-05-30 18:32:27 +0200

    Add quality metrics module

--- ofua/__init__.py
+++ ofua/__init__.py
@@ -2,4 +2,5 @@ from .asset_utils import detect_asset_type
 from .hybrid_upscaler import HybridUpscaler, UpscalerEngine, AssetProfile
 from .engines.upscayl_engine import UpscaylEngine
 from .engines.comfyui_engine import ComfyUIEngine
+from .quality_metrics import QualityMetrics
 
